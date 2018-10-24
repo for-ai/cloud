e1e94dca19af2b640317af54d4de9e260e3918d4
Aidan Gomez <aidan.n.gomez@gmail.com>
2018-10-24 10:57:17 +0100

    update

--- cloud/__init__.py
+++ cloud/__init__.py
@@ -1,5 +1,3 @@
-from cloud.envs import aws
-from cloud.envs import azure
 from cloud.envs import registry
 
 from cloud.envs import env
@@ -7,10 +5,17 @@ from cloud.envs.env import Instance
 from cloud.envs.env import Resource
 from cloud.envs.env import ResourceManager
 
+from cloud.envs import aws
+from cloud.envs.gcp import AWSInstance
+
+from cloud.envs import azure
+from cloud.envs.gcp import AzureInstance
+
 from cloud.envs import gcp
 from cloud.envs.gcp import GCPInstance
 from cloud.envs.gcp import TPU
 from cloud.envs.gcp import TPUManager
 
 from cloud.cloud import connect
-from cloud.cloud import disconnect
+from cloud.cloud import down
+from cloud.cloud import delete
