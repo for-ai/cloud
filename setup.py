f5b6114c2b565ab3a0776503f77a86e8d3d297e0
Aidan Gomez <hello@aidangomez.ca>
2020-01-06 15:39:00 -0500

    Update PyPi version number (#25)
    * fix zone issue with TPUs
    
    * update version number

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from setuptools import setup, find_packages
 from os import path
+
+from setuptools import find_packages, setup
 
 here = path.abspath(path.dirname(__file__))
 
@@ -8,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
   long_description = f.read()
 
 setup(name='dl-cloud',
-      version='0.1.4',
+      version='0.1.5',
       description='Cloud resource management for deep learning applications.',
       long_description=long_description,
       long_description_content_type='text/markdown',
