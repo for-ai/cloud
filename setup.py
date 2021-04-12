b5f56e550a8f3e257f35c5b4a45caec2c0ba97db
Aidan Gomez <hello@aidangomez.ca>
2021-04-12 17:19:23 -0400

    Update to 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='dl-cloud',
-      version='0.2.0',
+      version='0.2.1',
       description='Cloud resource management for deep learning applications.',
       long_description=long_description,
       long_description_content_type='text/markdown',
